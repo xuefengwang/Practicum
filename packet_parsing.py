788e012f8d33c2319dce0efae0e62e1db3ee7c6f
William Wang <xuefengw@gmail.com>
2021-09-29 16:20:06 -0400

    m

--- packet_parsing.py
+++ packet_parsing.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 from scapy.all import *
+import os
 from pathlib import Path
+
 home = str(Path.home())
+PCAP_FOLDER = "wlan0_pcap"
+
+# The captured pcap files are located at $HOME/wlan0_pcap folder. 
+# Any file in that folder is assumed to have not been processed. 
+def next_pcap_file():
+  dir = os.path.join(home, PCAP_FOLDER)
+  paths = sorted(Path(dir).iterdir(), key=os.path.getmtime)
+  for p in paths:
+    if os.path.isfile(p):
+      return p
+    
 
 def parse_packet(pkt):
-    print(pkt.summary())
+  print(pkt.summary())
 
-sniff(offline=f'{home}/wlan0_pcap/wlan0_2021-09-28_21.32.47.pcap', prn=parse_packet, store=0)
+pcap_file = next_pcap_file()
+
+print(f"processing {pcap_file}")
+sniff(count=10, offline=str(pcap_file), prn=parse_packet, store=0)
