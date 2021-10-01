b3701e2416cf6895cebd9275321f35e98c59b86d
William Wang <xuefengw@gmail.com>
2021-09-30 20:09:47 -0400

    m

--- packet_parsing.py
+++ packet_parsing.py
@@ -2,14 +2,19 @@
 from scapy.all import *
 import os
 from pathlib import Path
+import mysql.connector
 
-home = str(Path.home())
+HOME = str(Path.home())
 PCAP_FOLDER = "wlan0_pcap"
+db_conn
+
+def connect_db():
+  
 
 # The captured pcap files are located at $HOME/wlan0_pcap folder. 
 # Any file in that folder is assumed to have not been processed. 
 def next_pcap_file():
-  dir = os.path.join(home, PCAP_FOLDER)
+  dir = os.path.join(HOME, PCAP_FOLDER)
   paths = sorted(Path(dir).iterdir(), key=os.path.getmtime)
   for p in paths:
     if os.path.isfile(p):
@@ -18,7 +23,12 @@ def next_pcap_file():
 def parse_packet(pkt):
   print(pkt.summary())
 
+def clean_up(pcap_file):
+  os.rename(pcap_file, os.path.join(HOME, PCAP_FOLDER, "processed", pcap_file.name))
+
 pcap_file = next_pcap_file()
 
 print(f"processing {pcap_file}")
 sniff(count=10, offline=str(pcap_file), prn=parse_packet, store=0)
+
+clean_up(pcap_file)
