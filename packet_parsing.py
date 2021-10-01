23a883df01e84a203adee7e5edb3979ae241e22f
William Wang <xuefengw@gmail.com>
2021-10-01 16:04:31 -0400

    m

--- packet_parsing.py
+++ packet_parsing.py
@@ -5,14 +5,17 @@ from pathlib import Path
 import mysql.connector
 import configparser
 
-
 HOME = str(Path.home())
 PCAP_FOLDER = "wlan0_pcap"
-config = configparser.ConfigParser()
-config.read("packet_parsing.ini")
-db_config = dict(config.items('Database'))
-db_conn = mysql.connector.connect(**db_config)
-print(f"db conn {db_conn.connection_id}") 
+db_conn = None
+
+def setup():
+  global db_conn
+  config = configparser.ConfigParser()
+  config.read("packet_parsing.ini")
+  db_config = dict(config.items('Database'))
+  db_conn = mysql.connector.connect(**db_config)
+  print(f"db conn: {db_conn.connection_id}") 
 
 # The captured pcap files are located at $HOME/wlan0_pcap folder. 
 # Any file in that folder is assumed to have not been processed. 
@@ -29,6 +32,7 @@ def parse_packet(pkt):
 def clean_up(pcap_file):
   os.rename(pcap_file, os.path.join(HOME, PCAP_FOLDER, "processed", pcap_file.name))
 
+setup()
 pcap_file = next_pcap_file()
 
 print(f"processing {pcap_file}")
