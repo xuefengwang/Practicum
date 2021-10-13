2a788982b2095efc26a473281feeb1734c61d25e
William Wang <xuefengw@gmail.com>
2021-10-12 21:45:59 -0400

    checkpoint

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const config = require(process.env['HOME'] + 'iot_api.js');
 
 const indexRouter = require('./routes/index');
 
